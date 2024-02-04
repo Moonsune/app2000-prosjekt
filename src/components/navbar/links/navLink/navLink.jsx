69839de31b101f108002cf8cd514e4e82645cdb5
Bruggoli <99993823+Bruggoli@users.noreply.github.com>
2024-02-04 21:52:42 +0100

    we try again

--- src/components/navbar/links/navLink/navLink.jsx
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -1,22 +1,23 @@
 "use client";
 
-import { Component } from 'react';
-import styles from './navLink.module.css';
+//import { Component } from 'react'; // This import seems unnecessary since you're using a functional component
+import Link from 'next/link'; // Ensure you import Link from 'next/link'
+import styles from './NavLink.module.css'; // Assuming the CSS module is correctly named and imported
 import { usePathname } from 'next/navigation';
 
-const navLink = ( {item} ) => {
-
+const NavLink = ({ item }) => {
     const pathName = usePathname();
 
     return (
-        <Link href={item.path} className={`${ styles.container }` `${ 
-            pathName === item.path && styles.active
-            }`}
+        <Link 
+            href={item.path} 
+            className={`${styles.container} ${pathName === item.path ? styles.active : ''}`}
         > 
             {item.title} 
         </Link>
     );
 };
 
-export default navLink;
+export default NavLink;
+
 
