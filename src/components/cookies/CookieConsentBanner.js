fad8c35d310a18a6de577c6f95172cb769958002
Moonsune <113191400+Moonsune@users.noreply.github.com>
2024-04-15 17:58:14 +0200

    mer funksjonell cookies

--- src/components/cookies/CookieConsentBanner.js
+++ src/components/cookies/CookieConsentBanner.js
@@ -1,31 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import styles from "./CookieConsentBanner.module.css"; // Import CSS module
+import styles from "./CookieConsentBanner.module.css";
 
 const CookieConsentBanner = () => {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      // Remove the cookie
-      document.cookie = 'myCookieConsent=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-      // Trigger a reload to show the banner again
-      window.location.reload();
-    }, 10000); // 10 seconds
-
-    // Clean up the interval when the component unmounts
-    return () => clearInterval(intervalId);
+   
+    const acceptedCookies = document.cookie.includes('myCookieConsent=true');
+    if (acceptedCookies) {
+      setIsVisible(false);
+    }
   }, []);
 
   const handleUnderstand = () => {
-    // Logic for understanding cookies
     console.log("Cookies understood.");
-    setIsVisible(false); // Hide the banner
+   
+    document.cookie = 'myCookieConsent=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+    setIsVisible(false); 
   };
 
   const handleDecline = () => {
-    // Logic for declining cookies
     console.log("Cookies declined.");
-    setIsVisible(false); // Hide the banner
+    setIsVisible(false); 
   };
 
   return (
