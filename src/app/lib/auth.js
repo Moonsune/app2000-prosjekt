71e3b143147861b2c527bc2b11e5fcaede9d2cc4
Bruggoli <99993823+Bruggoli@users.noreply.github.com>
2024-04-17 17:50:54 +0200

    vercel check

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -2,12 +2,28 @@
 
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 
 export const { handlers:{GET, POST}, auth, signIn, signOut } = NextAuth({
     providers: [
-        GitHub({ 
-            clientId: process.env.GITHUB_ID, 
-            clientSecret: process.env.GITHUB_SECRET 
-        })],
-})
-
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+            authorization: {
+                params: { scope: 'read:user' }
+            }
+        }),
+    ],
+    callbacks: {
+        redirect: async ({ url, baseUrl }) => {
+            return baseUrl + '/'; // Ensure `baseUrl` is correctly determined.
+        },
+    },
+    pages: {
+        signIn: '/auth/signin',
+    },
+    secret: process.env.AUTH_SECRET,
+    session: {
+        strategy: 'jwt',
+    },
+});
